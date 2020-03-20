99e0807fc78216ddb5c90b432ad6b0ad0687c8c3
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-03-19 23:56:39 -0400

    added mlab

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://<dbuser>:<dbpassword>@ds143191.mlab.com:43191/heroku_5hdlnqjx",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/htmlRoutes.js"));
 
